cb036e3fc505926f6a625fbe323b9798b9f9879f
therealzeek <161908536+therealzeek@users.noreply.github.com>
2024-03-03 02:48:20 -0500

    Update iconshow.lua

--- iconshow.lua
+++ iconshow.lua
@@ -1,28 +1,12 @@
-local UserInputService = game:GetService("UserInputService")
+local keytoclick = getgenv().keytoclick or "V" -- Default key is "V"
+local vim = game:GetService("VirtualInputManager")
 
--- Function to handle the key press event
-local function onKeyPress(input)
-    if input.KeyCode == Enum.KeyCode.V then
-        -- Call the function to perform the desired action directly
-        performAction()
-    end
-end
-
--- Connect the key press event to the function
-UserInputService.InputBegan:Connect(onKeyPress)
-
--- Function to perform the desired action
-local function performAction()
-    -- Add your desired action here
-    print("Key 'V' pressed or image button clicked!")
-end
-
--- Rest of your code
 local ScreenGui = Instance.new("ScreenGui")
 local Frame = Instance.new("Frame")
 local ImageButton = Instance.new("ImageButton")
 
---Properties:
+-- Properties:
+
 ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
@@ -37,9 +21,12 @@ ImageButton.Parent = Frame
 ImageButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 ImageButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
 ImageButton.BorderSizePixel = 0
-ImageButton.Position = UDim2.new(0.579999983, 0, 0.680000007, 0)
+ImageButton.Position = UDim2.new(0.5, -19.5, 0.5, -19.5) -- Center the button
 ImageButton.Size = UDim2.new(0, 39, 0, 39)
-ImageButton.Image = "http://www.roblox.com/asset/?id=16599489351"
+ImageButton.Image = "http://www.roblox.com/asset/?id=16599489351" -- Make sure the image URL is correct
 
--- Connect the ImageButton click event to perform the action
-ImageButton.MouseButton1Click:Connect(performAction)
+-- Optional: Connect a function to the MouseButton1Click event to perform an action when the button is clicked
+ImageButton.MouseButton1Click:Connect(function()
+    print("ImageButton clicked!")
+    vim:SendKeyEvent(true, keytoclick, false, game)
+end)
