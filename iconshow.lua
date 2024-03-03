1cfc4320e966ff3fd5e1cb526427deb1beae8cd9
therealzeek <161908536+therealzeek@users.noreply.github.com>
2024-03-03 02:12:09 -0500

    Update iconshow.lua

--- iconshow.lua
+++ iconshow.lua
@@ -6,26 +6,23 @@ local vim = game:GetService("VirtualInputManager")
 
 -- Function to handle button click
 local function onButtonClick()
-    -- Send the key event for V directly
-    vim:SendKeyEvent(true, Enum.KeyCode.V, false, game)
+    -- Send the key event for the keytoclick value directly
+    vim:SendKeyEvent(true, Enum.KeyCode[keytoclick], false, game)
 end
 
 -- Function to create the GUI
 local function createGUI()
     -- Create ScreenGui, Frame, and ImageButton
     local ScreenGui = Instance.new("ScreenGui")
-    ScreenGui.Parent = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
-    ScreenGui.IgnoreGuiInset = true
-    ScreenGui.DisplayOrder = 999
+    ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
     ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
     local Frame = Instance.new("Frame")
     Frame.Parent = ScreenGui
     Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    Frame.BackgroundTransparency = 1
+    Frame.BackgroundTransparency = 1.000
     Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
     Frame.BorderSizePixel = 0
-    Frame.Position = UDim2.new(0.5, -50, 0.5, -50) -- Center the Frame
     Frame.Size = UDim2.new(0, 100, 0, 100)
 
     local ImageButton = Instance.new("ImageButton")
@@ -33,23 +30,17 @@ local function createGUI()
     ImageButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
     ImageButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
     ImageButton.BorderSizePixel = 0
-    ImageButton.Size = UDim2.new(1, 0, 1, 0)
+    ImageButton.Position = UDim2.new(0.579999983, 0, 0.680000007, 0)
+    ImageButton.Size = UDim2.new(0, 39, 0, 39)
     ImageButton.Image = "http://www.roblox.com/asset/?id=16599489351"
 
     -- Connect button click event to the function
     ImageButton.MouseButton1Click:Connect(onButtonClick)
-
-    -- Keep GUI visible
-    game:GetService("RunService").RenderStepped:Connect(function()
-        ScreenGui.Enabled = true
-    end)
 end
-
-
 
 -- Function to check if the GUI exists
 local function checkGUI()
-    local ScreenGui = game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("ScreenGui")
+    local ScreenGui = game.Players.LocalPlayer.PlayerGui:FindFirstChild("ScreenGui")
     if not ScreenGui then
         createGUI()
     end
@@ -60,5 +51,3 @@ while true do
     wait(1)
     checkGUI()
 end
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/therealzeek/Ctool/main/README.lua", true))()
